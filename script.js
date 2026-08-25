a37c88390455aeb6093a9a88cd4929895d3734ca
sutthimas32-png <sutthimas32@gmail.com>
2026-08-25 09:04:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document addeventlistener('DOMContentLoaded',function () {
+    document.getElementById('year').innerHTML = new Daet().getFullyYear();
     console.log('Protfolio ของ Sutthimas พร้อมแล้ว!');
 });
